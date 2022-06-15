82cedee3341a9b3c66e29dd416ffa33c6a229ef2
Jfontaine <jfontaine19@gmail.com>
2022-06-15 12:34:07 -0400

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC5ISWOdhA3cqg5CgeUjVoos8j5dj2oOn8",
+  authDomain: "bidmediscourse.firebaseapp.com",
+  projectId: "bidmediscourse",
+  storageBucket: "bidmediscourse.appspot.com",
+  messagingSenderId: "794179856055",
+  appId: "1:794179856055:web:3ba7d2a386388e853b0f32",
+  measurementId: "G-C6YQ6G1FYC",
+};
