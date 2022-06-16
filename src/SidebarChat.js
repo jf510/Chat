731aff8d187b1efe857738afa0e8ea9589f02002
Jfontaine <jfontaine19@gmail.com>
2022-06-15 21:48:47 -0400

    Added function to create new chat. Make the SidebarChat component reusable

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -2,20 +2,32 @@ import { Avatar } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import "./SidebarChat.css";
 
-function SidebarChat() {
+function SidebarChat({ addNewChat }) {
   const [seed, setSeed] = useState("");
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 1000));
   }, []);
 
-  return (
+  const createChat = () => {
+    const roomName = prompt("Please enter a chat room name");
+
+    if (roomName) {
+      // do some database stuff here
+    }
+  };
+
+  return !addNewChat ? (
     <div className="sidebarChat">
       <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
       <div className="sidebarChat__info">
         <h2>Room Name</h2>
         <p>last message...</p>
       </div>
+    </div>
+  ) : (
+    <div onClick={createChat} className="sidebarChat">
+      <h2>Add new Chat</h2>
     </div>
   );
 }
