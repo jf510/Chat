8affb0c88b5841a3df83b54c1a6b4388be150b70
Jfontaine <jfontaine19@gmail.com>
2022-06-16 21:01:52 -0400

    Added react router. Made paths for chat depending on logged in status

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import "./App.css";
 import Chat from "./Chat";
+import Home from "./Home";
 import Sidebar from "./Sidebar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <div className="app__body">
-        {/** Sidebar */}
-        <Sidebar />
-
-        {/** Chat */}
-        <Chat />
+    <Router>
+      <div className="app">
+        <div className="app__body">
+          <Sidebar />
+          <Routes>
+            <Route path="/rooms/:roomId" element={<Chat />}></Route>
+            <Route path="/" ></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
